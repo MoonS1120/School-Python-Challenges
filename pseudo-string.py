c31752b99c3d0cade4191b74a10128f2dc8d8e67
MoonS <99530280+MoonS1120@users.noreply.github.com>
2023-09-11 14:01:51 +0800

    Update pseudo-string.py

--- pseudo-string.py
+++ pseudo-string.py
@@ -1,37 +1,37 @@
 def LEFT(string,length):   
-    return string[:length]
+  return string[:length]
 
 def RIGHT(string,length):
-    return string[-1:-length:-1]
+  return string[-1:-length:-1]
 
 def MID(string,start,end):
-    return string[start:end]
+  return string[start:end]
     
 def LENGTH(string):
-    return len(string)
+  return len(string)
 
 def LCASE(character):
-    if character.islower():
-      return character
+  if character.islower():
+    return character
     
 def UCASE(character):
-    if character.isupper():
-      return character
+  if character.isupper():
+    return character
 
 def TO_UPPER(string):
-    return string.upper()
+  return string.upper()
 
 def TO_LOWER(string):
-    return string.lower()
+  return string.lower()
  
 def NUM_TO_STRING(num):
-    return str(num)
+  return str(num)
 
 def STRING_TO_NUM(str):
-    return int(str)
+  return int(str)
 
 def ASC(character):
-    return ord(character)
+  return ord(character)
 
 print(LEFT("hello",2))
 print(TO_UPPER("hello"))
